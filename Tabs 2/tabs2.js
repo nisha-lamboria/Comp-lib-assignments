d5b0d12c048f4134f84dca2b54758a9b6dce151f
nisha4545 <88261286+nisha4545@users.noreply.github.com>
2022-01-27 16:37:21 +0530

    tabs 1st layout done

--- Tabs 2/tabs2.js
+++ Tabs 2/tabs2.js
@@ -1,1 +1,20 @@
-let tabs= document.getElementsByClassName('')
+let tabLinks= document.querySelectorAll('a')
+// let activeTabs=document.querySelectorAll('tabs-list.active')
+let tabs=document.querySelectorAll('.tab')
+let arrayTabLinks=Array.from(tabLinks)
+
+
+Array.from(tabLinks).forEach((tabLink)=>{
+    tabLink.addEventListener("click",(e)=>{
+        console.log('clicked')
+        e.preventDefault()
+        console.log(e.target)
+        let activeElems=document.querySelectorAll('.active')
+        console.log(activeElems)
+        activeElems.forEach(el=>{
+            el.classList.remove('active')
+        })
+        console.log(activeElems)
+        e.target.classList.add('active')
+    })
+})
